5ff90f66907b0082e8c348745f6c7865d9c515c6
Maximilian Klein <13904220+KleinDevDE@users.noreply.github.com>
2025-12-21 01:51:26 +0100

    Add route for listing puzzles and update composer dependencies

--- Modules/Puzzles/routes/web.php
+++ Modules/Puzzles/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Puzzles\Http\Controllers\PuzzlesController;
 
 Route::middleware(['auth'])->group(function () {
     Route::prefix('/puzzles')->name('modules.puzzles.')->group(function () {
+        Route::get('/list', [PuzzlesController::class, 'list'])->name('list');
         Route::get('/{any?}', function () {
             return view('puzzles::app');
         })->where('any', '.*')->name('spa');
