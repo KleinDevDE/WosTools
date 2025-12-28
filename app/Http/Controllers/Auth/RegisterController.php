3240fdfb72ee901815b9cd62c3f9503a79a716e6
Maximilian Klein <13904220+KleinDevDE@users.noreply.github.com>
2025-12-28 05:43:58 +0100

    Enhance registration flow by adding account activation check and updating user password

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,12 +7,13 @@ use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use App\Models\UserInvitation;
 use Auth;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class RegisterController extends Controller
 {
-    public function show(Request $request): View
+    public function show(Request $request): RedirectResponse|View
     {
         if (!$request->hasValidSignature(true)) {
             return view('auth.register')->withErrors(['token' => 'Could not determine registration token, please insert it manually.']);
@@ -21,6 +22,10 @@ class RegisterController extends Controller
         $userInvitation = UserInvitation::where('token', $request->token)->first();
         if (!$userInvitation) {
             return view('auth.register')->withErrors(['token' => 'Invalid registration token!']);
+        }
+
+        if ($userInvitation->status === UserInvitation::STATUS_ACCEPTED) {
+            return redirect()->route('auth.login')->withErrors(['username' => 'Your account has already been activated, please login.']);
         }
 
         return view('auth.register', ['userInvitation' => $userInvitation]);
@@ -33,7 +38,7 @@ class RegisterController extends Controller
             return redirect()->back()->withInput($registerRequest->only(['username', 'token']))->withErrors(['token' => 'Invalid registration token!']);
         }
 
-        $userInvitation->user->update(['status' => User::STATUS_ACTIVE, 'last_login_at' => now()]);
+        $userInvitation->user->update(['status' => User::STATUS_ACTIVE, 'last_login_at' => now(), 'password' => ($registerRequest->password)]);
         $userInvitation->status = UserInvitation::STATUS_ACCEPTED;
         $userInvitation->accepted_at = now();
         $userInvitation->save();
