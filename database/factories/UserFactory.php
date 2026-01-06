71aa88b1a3f56650928dc62f7eeaa6321f926a9f
Maximilian Klein <13904220+KleinDevDE@users.noreply.github.com>
2026-01-06 04:24:45 +0100

    Fixed seeder issues

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,15 +25,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'player_id' => fake()->unique()->numberBetween(10,10000),
-            'player_name' => fake()->unique()->userName(),
+            'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make(hash('sha256', fake()->userName())),
             'remember_token' => Str::random(10),
             'last_login_at' => fake()->dateTime(),
-            'status' => fake()->randomElement(User::STATUS_VALUES),
-            'is_virtual' => true
         ];
     }
 
